aad226077ee6418c48a4b58ccab470484ae64adc
Aniss <aniss1.outaleb@epita.fr>
2025-09-11 14:14:43 +0200

    deployment & new port

--- test_server.py
+++ test_server.py
@@ -1,6 +1,6 @@
 import requests
 
 # host = "http://ubuntu@4.210.225.87:8088"
-host = "http://localhost:8088"
+host = "http://localhost:8002"
 r = requests.post(f"{host}/predict", json={"size": 100, "nb_rooms": 3, "garden": True})
 print(r.json())
